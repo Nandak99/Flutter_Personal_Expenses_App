c27a2fb49d9cc2be8bb1356b9fbee14b3f99786d
Nandak99 <54415494+Nandak99@users.noreply.github.com>
2022-03-11 21:39:31 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -28,3 +28,37 @@ void main() {
     expect(find.text('1'), findsOneWidget);
   });
 }
+import 'package:Flutter_Personal_Expenses_App/widget_test.dart';
+import 'dart:convert';
+import 'package:test/test.dart';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+
+void main() {
+  final provider = NewsApiProvider();
+
+  test('Sample test', () {
+    // SETUP
+    final sum = 1 + 3;
+    // EXPECTATION
+    expect(sum, 4);
+  });
+
+  test('Fetch ids from the api call', () async {
+    final mockClient = MockClient((request) async {
+      return Response(json.encode([1, 2, 3, 4]), 200);
+    });
+    provider.client = mockClient;
+    final ids = await provider.fetchTopIds();
+    expect(ids, [1, 2, 3, 4]);
+  });
+
+  test('Fetch item returns a item model', () async {
+    final mockClient = MockClient((request) async {
+      final jsonMap = {'id': 123};
+      return Response(json.encode(jsonMap), 200);
+    });
+    provider.client = mockClient;
+    final item = await provider.fetchItem(1);
+    expect(item.id, 123);
+  });
